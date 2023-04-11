be87165830e333ce3683d2f6823817350d60e7db
Kaustav Ghosh Dastidar <kaustav.gd@outlook.com>
2023-04-11 22:27:48 +0500

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import index, profile, RegisterView
+from .views import index,home, profile, RegisterView
 
 urlpatterns = [
-    path('',index, name='index'),
+    path('',home, name='login'),
     path('register/', RegisterView.as_view(), name='users-register'),
     path('profile/', profile, name='users-profile'),
-    #path('index/', index, name='index'),
+    path('index/', index, name='index'),
 ]

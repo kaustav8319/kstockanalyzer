e0ca317f6bd24d8881dd041ca058fa825f3c7ec6
Kaustav Ghosh Dastidar <kaustav.gd@outlook.com>
2023-04-11 21:57:04 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '69qb2zq)4az8=00rmeae4vj_-1@&b%gys@fgu@*h*-1n74)bcf'
+SECRET_KEY = 'django-insecure-d*2eog&xs-3+50xd0c(i+636^(fs38*si4df68-fn80%kf#+%%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 DEBUG_PROPAGATE_EXCEPTIONS = True
 ALLOWED_HOSTS = ["kstockanalyzer.herokuapp.com","127.0.0.1"]
 

c7510ebab771ba0cb250eb8e6f7a8a2e4fe1b93c
Kaustav Ghosh Dastidar <kaustav.gd@outlook.com>
2023-04-11 22:17:10 +0500

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -26,29 +26,7 @@ from app.views import CustomLoginView, ResetPasswordView, ChangePasswordView
 from app.forms import LoginForm
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    path('', include('app.urls')),
-
-    path('login/', CustomLoginView.as_view(redirect_authenticated_user=True, template_name='login.html',
-                                           authentication_form=LoginForm), name='login'),
-
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
-
-    path('password-reset/', ResetPasswordView.as_view(), name='password_reset'),
-
-    path('password-reset-confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
-         name='password_reset_confirm'),
-
-    path('password-reset-complete/',
-         auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
-         name='password_reset_complete'),
-
-    path('password-change/', ChangePasswordView.as_view(), name='password_change'),
-
-    re_path(r'^oauth/', include('social_django.urls', namespace='social')),
-
+    
 
 
     path('', index),

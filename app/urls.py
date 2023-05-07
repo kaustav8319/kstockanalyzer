a8ac701f685fba5e3282d2a12fbc282e444caa8e
kaustav8319 <kaustav.gd@outlook.com>
2023-05-07 16:31:13 +0500

    little code changes

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import index,home, profile, RegisterView
 
 urlpatterns = [
-    path('',home, name='users-home'),
+    path('',home, name='login'),
     path('register/', RegisterView.as_view(), name='users-register'),
     path('profile/', profile, name='users-profile'),
     path('index/', index, name='index'),

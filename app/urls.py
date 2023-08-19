d4fa7aff3762745c5560ce2a99b1b67cf0d846d9
Arnab-Karmakar21 <karmakararnab.2017@gmail.com>
2023-08-19 11:34:29 +0530

    make registration page as landing page

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from .views import index,home, profile, RegisterView
 
 urlpatterns = [
-    path('',home, name='login'),
-    path('register/', RegisterView.as_view(), name='users-register'),
+    # path('',home, name='login'),
+    # path('register/', RegisterView.as_view(), name='users-register'),
+    path('', RegisterView.as_view(), name='users-register'),
     path('profile/', profile, name='users-profile'),
     path('index/', index, name='index'),
 ]
